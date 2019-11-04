d0c078cddf35dc9b7fc377ac3fd8cb5fa3a39293
Shilpa <shilpa@Shilpas-MacBook-Pro.local>
2019-11-03 19:02:56 -0600

    Displaying all movies

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,45 @@
-import React from 'react'
-import logo from '../../src/logo.svg'
+import React, { useState, useEffect } from 'react'
 import '../../src/App.css'
+import Header from '../../src/components/Header'
+import Movie from '../../src/components/Movie'
 
-function App() {
+
+const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY
+const MOVIE_API_URL = `https://www.omdbapi.com/?s=man&apikey=${MOVIE_API_KEY}`
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === 'True') {
+          setMovies(jsonResponse.Search)
+          setLoading(false)
+        } else {
+          setErrorMessage(jsonResponse.Error)
+          setLoading(false)
+        }
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text='HOOKED' />
+        <p className='App-intro'> Sharing a few of my favorite movies</p>
+        <div className='movies'>
+          {loading && !errorMessage 
+          ?  (<span>Loading...</span>)
+          : errorMessage 
+            ? (<div className='errorMessage'>{errorMessage}</div>)
+            : ( movies.map((movie, index) => (
+              <Movie key={`${index}-${movie.Title}`} movie={movie}/>
+            )))
+          }
+        </div>
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,19 +4,19 @@ import React from 'react'
 const DEFAULT_PLACEHOLDER_IMAGE = 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg'
 
 const Movie = ({ movie }) => {
-     const poster = movie.poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.poster
+     const poster = movie.poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster
 
      return (
          <div className='movie'>
-             <h2>{movie.title}</h2>
+             <h2>{movie.Title}</h2>
              <div>
                 <img
                     width='200'
-                    alt={movie.title}
+                    alt={movie.Title}
                     src={poster}
                 />
              </div>
-             <p>({movie.year})</p>
+             <p>({movie.Year})</p>
          </div>
      )
 }
