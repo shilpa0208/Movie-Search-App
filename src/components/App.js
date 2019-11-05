f1a1331b75bf34b34cf0908e5b339790d04bcde2
Shilpa <shilpa@Shilpas-MacBook-Pro.local>
2019-11-05 07:03:03 -0600

    Search component is now embedded by the App

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 import '../../src/App.css'
 import Header from '../../src/components/Header'
 import Movie from '../../src/components/Movie'
+import Search from '../../src/components/Search'
 
 
 const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY
-const MOVIE_API_URL = `https://www.omdbapi.com/?s=man&apikey=${MOVIE_API_KEY}`
+const MOVIE_API_BASE_URL = 'https://www.omdbapi.com/'
 
 const App = () => {
   const [loading, setLoading] = useState(true)
@@ -13,7 +14,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    fetch(MOVIE_API_URL)
+    fetch(MOVIE_API_BASE_URL + `?s=man&apikey=${MOVIE_API_KEY}`)
       .then(response => response.json())
       .then(jsonResponse => {
         if (jsonResponse.Response === 'True') {
@@ -26,18 +27,36 @@ const App = () => {
       })
   }, [])
 
+  const search = (searchValue) => {
+    setLoading(true)
+    setErrorMessage(null)
+
+    fetch(MOVIE_API_BASE_URL + `?s=${searchValue}&apiKey=${MOVIE_API_KEY}`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === 'True') {
+          setMovies(jsonResponse.Search)
+          setLoading(false)
+        } else {
+          setErrorMessage(jsonResponse.Error)
+          setLoading(false)
+        }
+      })
+  }
+
   return (
     <div className="App">
-      <Header text='HOOKED' />
+      <Header text='MOVIE SEACH APP' />
+        <Search search={search} />
         <p className='App-intro'> Sharing a few of my favorite movies</p>
         <div className='movies'>
           {loading && !errorMessage 
-          ?  (<span>Loading...</span>)
+          ?  <span className='loading'>Loading...</span>
           : errorMessage 
-            ? (<div className='errorMessage'>{errorMessage}</div>)
-            : ( movies.map((movie, index) => (
+            ? <div className='errorMessage'>{errorMessage}</div>
+            : movies.map((movie, index) => (
               <Movie key={`${index}-${movie.Title}`} movie={movie}/>
-            )))
+            ))
           }
         </div>
     </div>
